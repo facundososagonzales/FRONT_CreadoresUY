2aac1be00bd31f46ae038e74ea835f1590c89638
Facundo Sosa <facu_12_sosa@hotmail.com>
2021-11-03 19:21:44 -0300

    validaciones en login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -15,6 +15,10 @@ export class LoginComponent implements OnInit {
   public checked:boolean;
   public subbmited:boolean=false
   public Success:boolean;
+  public loginForm:FormGroup;
+  public noRequiredPassword:boolean = true;
+  public noRequiredEmail:boolean=true;
+  public validEmail:boolean=true;
 
   constructor(private http:userServices, private router:Router) {}
 
@@ -23,41 +27,59 @@ export class LoginComponent implements OnInit {
     if(token!=null){
       this.router.navigate(['/home']);
     }
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('',Validators.required)
+    });
   }
 
   Login(email:string,password:string){
     this.subbmited=false;
+    this.noRequiredPassword = true;
+    this.noRequiredEmail=true;
+    this.validEmail=true;
     var user = new User();
     user.email = email;
     user.password = password;
-    this.http.UserLogin(user).subscribe(res =>{
-      console.log(res);
-      if(res["success"]){
-        this.Success=true;
-        const token = res["obj"]["token"];
-        if(token != null){
-          sessionStorage.setItem('token', token);
-          sessionStorage.setItem('name', res["obj"]["name"])
-          sessionStorage.setItem('userId', (res["obj"]["userId"]).toString())
-          sessionStorage.setItem('email', res["obj"]["email"])
-          if(res["obj"]["creatorId"] != 0){
-            sessionStorage.setItem('userType', "creator");
-            sessionStorage.setItem('creatorId', res["obj"].creatorId.toString())
-            this.router.navigate(['/user-profile']);
+    if(this.loginForm.valid){
+      this.http.UserLogin(user).subscribe(res =>{
+        console.log(res);
+        if(res["success"]){
+          this.Success=true;
+          const token = res["obj"]["token"];
+          if(token != null){
+            sessionStorage.setItem('token', token);
+            sessionStorage.setItem('name', res["obj"]["name"])
+            sessionStorage.setItem('userId', (res["obj"]["userId"]).toString())
+            sessionStorage.setItem('email', res["obj"]["email"])
+            if(res["obj"]["creatorId"] != 0){
+              sessionStorage.setItem('userType', "creator");
+              sessionStorage.setItem('creatorId', res["obj"].creatorId.toString())
+              this.router.navigate(['/user-profile']);
 
-          }else if(res["obj"]["isAdmin"]){
-            sessionStorage.setItem('userType', "admin")
-            this.router.navigate(['/backOffice']);
-          }else{
-            sessionStorage.setItem('userType', "user")
-            this.router.navigate(['/feed']);
+            }else if(res["obj"]["isAdmin"]){
+              sessionStorage.setItem('userType', "admin")
+              this.router.navigate(['/backOffice']);
+            }else{
+              sessionStorage.setItem('userType', "user")
+              this.router.navigate(['/feed']);
+            }
           }
+        }else{
+          this.Success=false;
         }
-      }else{
-        this.Success=false;
+        this.subbmited=true;
+      });
+    }else{
+      if(this.loginForm.get('password').errors!=null){
+        this.noRequiredPassword=false;
       }
-      this.subbmited=true;
-    });
+      if(this.loginForm.get('email').errors!=null && this.loginForm.get('email').errors.email){
+        this.validEmail=false;
+      }else if(this.loginForm.get('email').errors!=null && this.loginForm.get('email').errors.required){
+        this.noRequiredEmail=false;
+      }
+    }
   }
 
   getToken(){
