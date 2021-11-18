710a00a7bd047347fca2dd5ebd9c62098dfb3a18
Camila Cardozo <c.cardozo24@outlook.com>
2021-11-18 10:47:36 -0300

    siguiendo con el alta creador

--- src/app/components/signup-creador/signup-creador.component.ts
+++ src/app/components/signup-creador/signup-creador.component.ts
@@ -18,11 +18,7 @@ interface Banco{
 export class SignupCreadorComponent implements OnInit {
   focus;
   htmlContent = '';
-  mpagoasign: string;
-  mpagos: string[] = ['Banco', 'Paypal'];
-  mpagoseleccionado: string = 'Banco';
   public editor= ClassicEditor;
-  public payPalConfig: any;
   maxNo = false;
   amt = 0;
   
@@ -52,84 +48,10 @@ export class SignupCreadorComponent implements OnInit {
  
   
   ngOnInit(): void {
-    this.mpagoasign = this.mpagos[2];
-    this.payPalConfig = {
-      currency: "EUR",
-      clientId: "AR6r31Q1-UBExv3N0jKTOAJBVR0s7lFj9peXOxgf_1opCSdk9On_TtR2VVDdukqlOeSb2DYjYxyRWALR",
-      createOrder: data =>
-        <ICreateOrderRequest>{
-          intent: "CAPTURE",
-          purchase_units: [
-            {
-              amount: {
-                currency_code: "EUR",
-                value: "9.99",
-                breakdown: {
-                  item_total: {
-                    currency_code: "EUR",
-                    value: "9.99"
-                  }
-                }
-              },
-              items: [
-                {
-                  name: "Enterprise Subscription",
-                  quantity: "1",
-                  category: "DIGITAL_GOODS",
-                  unit_amount: {
-                    currency_code: "EUR",
-                    value: "9.99"
-                  }
-                }
-              ]
-            }
-          ]
-        },
-      advanced: {
-        commit: "true"
-      },
-      style: {
-        label: "paypal",
-        layout: "vertical"
-      },
-      onApprove: (data, actions) => {
-        console.log(
-          "onApprove - transaction was approved, but not authorized",
-          data,
-          actions
-        );
-        actions.order.get().then(details => {
-          console.log(
-            "onApprove - you can get full order details inside onApprove: ",
-            details
-          );
-        });
-      },
-      onClientAuthorization: data => {
-        console.log(
-          "onClientAuthorization - you should probably inform your server about completed transaction at this point",
-          data
-        );
-      },
-      onCancel: (data, actions) => {
-        console.log("OnCancel", data, actions);
-      },
-      onError: err => {
-        console.log("OnError", err);
-      },
-      onClick: (data, actions) => {
-        console.log("onClick", data, actions);
-      }
-    };
-    
   }
 
 
-  changeComboo(event) {
-    console.log('chnaged', event && event.value);
-  }
 
-  closeModal: string;
   
   constructor() {}
  
